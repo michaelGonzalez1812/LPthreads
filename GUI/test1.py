fb2878f2134ddadaf40a57c53a7f99cb3bcf3d30
Erick Cordero <eguicoro2@gmail.com>
2019-10-07 17:07:46 -0600

    read file

--- GUI/test1.py
+++ GUI/test1.py
@@ -1,4 +1,4 @@
-import time, threading
+import time, threading,_thread
 from tkinter import *
 from tkinter import messagebox
 
@@ -10,28 +10,32 @@ def movement(value, canvas, rect, tipo):
     canvas.move(rect, value, 0) 
     cordenadas = canvas.coords(rect)
 
-    if(cordenadas[0] >= 75.0):
+    if(cordenadas[0] >= 150.0):
         if(tipo == 0):
-            rect = canvas.create_rectangle(5, 5, 25, 25, fill="blue")
+            rect = canvas.create_rectangle(5, 5, 75, 25, fill="blue")
         elif(tipo == 1):
-            rect = canvas.create_rectangle(5, 5, 25, 25, fill="red")
+            rect = canvas.create_rectangle(5, 5, 75, 25, fill="red")
         else:
-            rect = canvas.create_rectangle(5, 5, 25, 25, fill="green")
+            rect = canvas.create_rectangle(5, 5, 75, 25, fill="green")
     canvas.after(100, movement, value, canvas, rect, tipo) 
     
+def read_file():
+    while(TRUE):
+        print("sdfsdf")
 
 mainWindow = Tk()
 mainWindow.geometry("500x500")
 mainWindow.title("Envio de Paquetes")
+_thread.start_new_thread( read_file,() )
 canvas1 = Canvas(mainWindow, width=500, height=100)
 canvas1.pack()
-rect1 = canvas1.create_rectangle(5, 5, 25, 25, fill="blue")
+rect1 = canvas1.create_rectangle(5, 5, 75, 25, fill="blue")
 canvas2 = Canvas(mainWindow, width=500, height=100)
 canvas2.pack()
-rect2 = canvas2.create_rectangle(5, 5, 25, 25, fill="red")
+rect2 = canvas2.create_rectangle(5, 5, 75, 25, fill="red")
 canvas3 = Canvas(mainWindow, width=500, height=100)
 canvas3.pack()
-rect3 = canvas3.create_rectangle(5, 5, 25, 25, fill="green")
+rect3 = canvas3.create_rectangle(5, 5, 75, 25, fill="green")
 mainWindow.after(300, movement,1,canvas1,rect1,0)
 mainWindow.after(300, movement,3,canvas2,rect2,1)
 mainWindow.after(300, movement,2,canvas3,rect3,2)
