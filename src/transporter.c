43ffc21db46aebaffad56b02cdb5e1f46afbcdb6
Michael <michael.gonzalez1812@gmail.com>
2019-10-15 22:32:48 -0600

    main file updated

---
+++ src/transporter.c
@@ -0,0 +1,20 @@
+/*************************************************************
+ * Tecnológico de Costa Rica
+ * Conceptos básicos de Sistemas Operativos
+ * Athors:
+ *      Michael Gonzalez Rivera
+ *      Erick Cordero Rojas
+ *      Victor Montero
+ * 
+ * Description:
+ *      Implementation of lpthread library
+ * ***********************************************************/
+
+#include <lpthread.h>
+
+int main(int argc, char** argv) {
+
+    //TODO: Not implemented yet
+
+    return 0;
+}
