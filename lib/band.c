0c9f2a8121188425fe8906c3edf8a7e7b4b91e02
vic6595 <vic9565@hotmail.com>
2019-10-22 00:38:12 -0600

    added package generation

--- lib/band.c
+++ lib/band.c
@@ -54,6 +54,7 @@ int do_in_background(void *arg) {
     while(band->on) {
         //TODO: choose when create pkgs
         new_pkgs = generate_pkgs(&pkg_counter);
+        printf("on background: new pkgs is %d\n", (sizeof(new_pkgs)/sizeof(*new_pkgs)));
         //TODO: add new elements to the queue
         direction = get_dir(band);
         next_pkg = (direction == LEFT_TO_RIGHT) ? 
--- lib/package.c
+++ lib/package.c
@@ -18,7 +18,19 @@
 #include <unistd.h>
 
 void pkg_init(package_t *pkg, int *pkg_counter) {
-    pkg->id = *pkg_counter++;
+    pkg -> id = *pkg_counter++;
+    int flag = 0;
+    //avoid a weight of zero
+    while (flag){
+        int temp_wght = get_random(10);
+        if (temp_wght!= 0){
+            pkg -> weight = 0;
+            flag = 1;
+        }
+    }
+    
+    //TODO: function to get the type given the confg file  
+    pkg -> type = 0;
 }
 
 float compute_time(int pkg_mole) {
@@ -30,17 +42,21 @@ float compute_time(int pkg_mole) {
 
 
 package_t* generate_pkgs(int *pkg_counter){
-    //TODO: to be implemented
+    //TODO: to be finished
     int side = get_side();
     int qntty = get_qntty();
     printf("on gen package: quantity is %d\n", qntty);
-    return 0;
 
-    //asi se hace la llamada para inicializar paquetes
-    package_t pkg;
-    pkg_init(&pkg, pkg_counter);
+    package_t *result = malloc(qntty*sizeof(package_t));
+    //Loop to create the defined amount of packages
+    for (int i =0; i< qntty;i++){
+        pkg_init(&result[i], &pkg_counter);
 
-    return NULL;
+    }
+    
+    
+
+    return result;
 }
 
 int get_side(){
@@ -52,8 +68,8 @@ int get_qntty(){
     //TODO: read distribution from config file
     //Hardwired distribution
     int arr[] = {7,6,5,5,4,4,4,3,3,3,3,2,2,2,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,4,4,5,6,7};
+    
     int index = get_random((sizeof(arr)/sizeof(*arr)));
-    //printf("on qtty: rndm is %d\n", (sizeof(arr)/sizeof(*arr)));
     int qntty = arr[index];
     
 }
